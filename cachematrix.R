5eea73457a3202cae9afe3e48cd23c7e6f466a3b
SHIKHAR ZUTSHI <shikharzutshi@gmail.com>
2020-04-06 18:19:44 +0530

    Revert "Update cachematrix.R"
    This reverts commit 14f1e2f99413d6dbb4fe1d872994bec2d8d10f9e.

--- cachematrix.R
+++ cachematrix.R
@@ -4,15 +4,12 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  j <- NULL
-  set <- function(y){
-  x <<- y
-  j <<- NULL
-  }
-  get <- function()x
-  setInverse <- function(inverse) j <<- inverse
-  getInverse <- function() j 
-  list(set = set, get = get, 
-  setInverse = setInverse, 
-  getInverse = getInverse)
+
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
 }
